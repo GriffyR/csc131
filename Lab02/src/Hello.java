5aae492ed7a25dc3ed96cb7b149102060c2035df
Ryan Griffiths <ryanbgriffiths97@gmail.com>
2026-03-12 12:42:46 -0700

    File returned to original status for initial pulls.

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -5,9 +5,9 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("304711836 - This line was added by Ryan Griffiths.");
-		System.out.println("304856773 - This line was added by Quang Phan.");
-		System.out.println("304510635 - This line was added by Amy Rocha.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
