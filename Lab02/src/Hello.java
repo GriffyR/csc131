f356e0de3b1ac1f795fcccf8ef84d29d25f81a96
EthanKHenning <150199069+EthanKHenning@users.noreply.github.com>
2026-03-12 20:46:29 -0700

    Hello.java
    Ethan's addition, if I did it wrong please ping me on disc

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
 		System.out.println("304711836 - This line was added by Ryan Griffiths.");
 		System.out.println("304510635 - This line was added by Amy Rocha");
 		System.out.println("304856773 - This line was added by Quang Phan");
+		System.out.println("303292392 - This line was added by Ethan Henning");
 		System.out.println("303292392 - This line was added by Vincent Nguyen");
 		System.out.println("303792957 - This line was added by Wade Goh.");
 		System.out.println("301942160 - This line was added by Emily Yang.");
